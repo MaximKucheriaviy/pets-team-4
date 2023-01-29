f75788b1679f2e5fa140db023d84e348192377b6
Maxim Kucheriaviy <maxim-k@i.ua>
2023-01-29 10:01:25 +0200

    default page added

--- src/SharedLayout/SharedLayot.jsx
+++ src/SharedLayout/SharedLayot.jsx
@@ -1,17 +1,14 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { MainContainer, MainTitle } from "./SharedLayout.styled";
 
 import { Header } from "../components/Header/Navigation/allNavigation/Header";
-export const SharedLayout = ({ title = null, children }) => {
+export const SharedLayout = () => {
   return (
-    <MainContainer>
-      {title && <MainTitle>{title}</MainTitle>}
+    <>
       <Header />
       <Suspense fallback={null}>
-        {children}
         <Outlet />
       </Suspense>
-    </MainContainer>
+    </>
   );
 };
