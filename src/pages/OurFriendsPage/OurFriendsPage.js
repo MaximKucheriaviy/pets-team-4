272b20fdd8fc9da9901f1e2b08927339fbc266f3
Natalia Kozak <90988795+Natalja21@users.noreply.github.com>
2023-01-29 12:06:17 +0200

    apiSponsors + OurFriends page  без карточки

--- src/pages/OurFriendsPage/OurFriendsPage.js
+++ src/pages/OurFriendsPage/OurFriendsPage.js
@@ -1,12 +1,27 @@
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
+import { getAllSponsors } from "./apiSponsors";
+import { useEffect, useState } from "react";
 
 export default function OurFriendsPage() {
+  const [sponsors, setSponsors] = useState([]);
+  useEffect(() => {
+    const fechSponsors = async () => {
+      try {
+        const { data } = await getAllSponsors();
+        console.log(data);
+        setSponsors(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fechSponsors();
+  }, []);
   return (
     <div>
-     <Helmet>
+      <Helmet>
         <title>OurFriendsPage</title>
       </Helmet>
-      <h3>Our Friends</h3>
-      </div>
-  )
+      <h3>Our Friends</h3>|
+    </div>
+  );
 }
