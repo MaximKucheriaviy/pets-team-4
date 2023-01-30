eb1f305fc5cbfa4fe1fe1ef1aa65219d2b4cfc2d
DeniOS777 <Denios777@gmail.com>
2023-01-30 18:45:12 +0200

    fix styles searchForm and added filter

--- src/components/NewsCard/NewsCard.styled.js
+++ src/components/NewsCard/NewsCard.styled.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
 export const Card = styled.li`
+  transition: box-shadow 250ms ease-in-out;
+
   @media (max-width: 767px) {
     &:not(:last-child) {
       margin-bottom: 40px;
     }
+  }
+
+  &:hover {
+    box-shadow: 7px 4px 14px rgba(49, 21, 4, 0.07);
   }
 
   &::before {
@@ -31,19 +37,21 @@ export const Card = styled.li`
 export const Title = styled.h3`
   margin-bottom: 16px;
   height: 66px;
+  font-family: "Manrope";
   font-weight: 700;
   font-size: 24px;
   line-height: 1.37;
   letter-spacing: -0.01em;
-  color: #111111;
+  color: ${({ theme }) => theme.colors.black};
   overflow: hidden;
 `;
 
 export const Text = styled.p`
   margin-bottom: 40px;
   height: 154px;
+  font-family: "Manrope";
   line-height: 1.37;
-  color: #111321;
+  color: ${({ theme }) => theme.colors.black};
   overflow: hidden;
 `;
 
@@ -54,13 +62,21 @@ export const Wrap = styled.div`
 `;
 
 export const Date = styled.span`
+  font-family: "Manrope";
   line-height: 1.37;
   color: rgba(17, 17, 17, 0.6);
 `;
 
 export const Link = styled.a`
+  font-family: "Manrope";
   font-weight: 500;
   line-height: 1.37;
   text-decoration-line: underline;
-  color: #f59256;
+  color: ${({ theme }) => theme.colors.accent};
+  transition: color 250ms ease-in-out;
+
+  &:hover,
+  :focus {
+    color: #e58145;
+  }
 `;
--- src/components/NewsList/NewsList.styled.js
+++ src/components/NewsList/NewsList.styled.js
@@ -1,28 +1,29 @@
 import styled from "styled-components";
 
 export const CardList = styled.ul`
-  margin: 0;
-  padding: 0;
-  list-style: none;
+  margin-top: 40px;
 
   @media (min-width: 768px) and (max-width: 959px) {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(368px, 1fr));
+    margin-top: 60px;
+    grid-template-columns: repeat(auto-fill, minmax(336px, 1fr));
     row-gap: 60px;
     column-gap: 32px;
   }
 
   @media (min-width: 960px) and (max-width: 1279px) {
     display: grid;
+    margin-top: 60px;
     row-gap: 60px;
     column-gap: 32px;
-    grid-template-columns: repeat(auto-fill, minmax(406px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(384px, 1fr));
   }
 
   @media (min-width: 1280px) {
     display: grid;
+    margin-top: 60px;
     row-gap: 60px;
     column-gap: 32px;
-    grid-template-columns: repeat(auto-fill, minmax(405px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(394px, 1fr));
   }
 `;
