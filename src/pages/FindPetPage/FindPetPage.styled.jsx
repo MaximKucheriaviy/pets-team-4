7d1cfaa74677b0a1cc8ea09a3b6255af348655f6
YuriiLeo <yuriileontenko@gmail.com>
2023-02-02 01:36:26 +0200

    додав компоненти

---
+++ src/pages/FindPetPage/FindPetPage.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+
+    margin-top: 28px;
+    margin-bottom: 20px;
+
+         @media (min-width: 768px) {
+       margin: 40px 0 60px 0;
+    };
+`;
