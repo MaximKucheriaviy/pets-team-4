720ae5ee249efe72fafd62014cffd1bce6f4e1d5
NikolayYevsovich <yevsovich.nick@gmail.com>
2023-02-05 16:40:07 +0200

    Add user pet modal component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { lazy } from 'react';
-import { SharedLayout } from './SharedLayout/SharedLayot';
-import { Route, Routes } from 'react-router-dom'
+import { lazy } from "react";
+import { SharedLayout } from "./SharedLayout/SharedLayot";
+import { Route, Routes } from "react-router-dom";
 
-import { Suspense } from 'react';
+import { Suspense } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { SelectIsLoadingUserStatus } from './redux/auth/autSelectors';
-import { current } from './redux/auth/auth-operation';
+import { SelectIsLoadingUserStatus } from "./redux/auth/autSelectors";
+import { current } from "./redux/auth/auth-operation";
+import AddUserPetModal from "./components/AddUserPetModal/AddUserPetModal";
 
-const Home = lazy(() => import("./pages/HomePage/HomePage"))
-const News = lazy(() => import("./pages/NewsPage/NewsPage"))
-const OurFriends = lazy(() => import("./pages/OurFriendsPage/OurFriendsPage"))
-const FindPet = lazy(() => import("./pages/FindPetPage/FindPetPage"))
+const Home = lazy(() => import("./pages/HomePage/HomePage"));
+const News = lazy(() => import("./pages/NewsPage/NewsPage"));
+const OurFriends = lazy(() => import("./pages/OurFriendsPage/OurFriendsPage"));
+const FindPet = lazy(() => import("./pages/FindPetPage/FindPetPage"));
 
-const Login = lazy(() => import("./pages/LoginPage/LoginPage"))
-const Register = lazy(() => import("./pages/RegisterPage/RegisterPage"))
-const User = lazy(() => import("./pages/UserPage/UserPage"))
+const Login = lazy(() => import("./pages/LoginPage/LoginPage"));
+const Register = lazy(() => import("./pages/RegisterPage/RegisterPage"));
+const User = lazy(() => import("./pages/UserPage/UserPage"));
 
-const NotFoundPage = lazy(() => import("./pages/NotFoundPages/NotFoundPages"))
-
-
-
+const NotFoundPage = lazy(() => import("./pages/NotFoundPages/NotFoundPages"));
 
 function App() {
   const dispatch = useDispatch();
@@ -30,31 +28,31 @@ function App() {
     dispatch(current());
   }, [dispatch]);
 
-
   return (
     <>
-      {isLoadingUser ? <p>....Loading</p> :
+      {isLoadingUser ? (
+        <p>....Loading</p>
+      ) : (
         <Suspense>
           <Routes>
-            <Route path='/' element={<SharedLayout />}>
-              <Route path='/' element={<Home />} />
-              <Route path='/news' element={<News />} />
-              <Route path='/notices/sell' element={<FindPet />} />
+            <Route path="/" element={<SharedLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/notices/sell" element={<FindPet />} />
 
-              <Route path='/friends' element={<OurFriends />} />
+              <Route path="/friends" element={<OurFriends />} />
 
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
 
               <Route path="/user" element={<User />} />
-
 
               <Route path="*" element={<NotFoundPage />} />
             </Route>
           </Routes>
+          <AddUserPetModal />
         </Suspense>
-      }
-
+      )}
     </>
   );
 }
