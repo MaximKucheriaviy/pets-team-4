d67dce310261aaaecb8f848e4317460efced091f
DeniOS777 <Denios777@gmail.com>
2023-02-06 12:05:20 +0200

    added a scroll to top page on findPet and ourfriend

--- src/pages/FindPetPage/FindPetPage.jsx
+++ src/pages/FindPetPage/FindPetPage.jsx
@@ -7,32 +7,36 @@ import { Wrapper } from "./FindPetPage.styled";
 
 import { getNoticesByCategory } from "../../services/apiNotices";
 import { useEffect, useState } from "react";
+import { SlArrowUp } from "react-icons/sl";
 import { useParams } from "react-router-dom";
-
+import { ButtonToTop } from "../../components/ButtonToTop";
+import { scrollTopPage } from "../../helpers/scrollUp";
 
 // token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2RkOGQwNTI5NTEyZGY5N2Q3ZjI3ZDIiLCJpYXQiOjE2NzU0NjM5NDF9.jcqhFXup9wp8GmICuMdMU_zVs2vs5zZVKfj7D04JceY"
 // id=63dd8d0529512df97d7f27d2
 
-  // const [favorite, setFavorite] = useState(false);
-  
-  // function handleClick() {
-  //   if (favorite === true) {
-  //     setFavorite(false);
-  //   } 
-  //     setFavorite(true);
-  // }
+// const [favorite, setFavorite] = useState(false);
+
+// function handleClick() {
+//   if (favorite === true) {
+//     setFavorite(false);
+//   }
+//     setFavorite(true);
+// }
+const PAGE_SCROLL_DOWN = 600;
 
 export default function FindPetPage() {
-const [notices, setNotices] = useState([]);
-const [error, setError] = useState(null);
-const [filter, setFilter] = useState("");
+  const [notices, setNotices] = useState([]);
+  const [error, setError] = useState(null);
+  const [filter, setFilter] = useState("");
+  const [scrollTop, setScrollTop] = useState(0);
 
-const { category } = useParams();
-  
+  const { category } = useParams();
+
   const handleFilter = (e) => {
     e.preventDefault();
     setFilter(e.target.value);
-  }
+  };
 
   //   const handleSubmit = (e) => {
   //   e.preventDefault();
@@ -50,11 +54,11 @@ const { category } = useParams();
   };
 
   const visibleNotices = filteredNotices();
-  
+
   useEffect(() => {
     (async () => {
       try {
-        const  data  = await getNoticesByCategory(category);
+        const data = await getNoticesByCategory(category);
         console.log(data);
         setNotices(data);
       } catch (error) {
@@ -62,6 +66,20 @@ const { category } = useParams();
       }
     })();
   }, [category]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollTop(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const isShowButtonTop = scrollTop > PAGE_SCROLL_DOWN;
 
   return (
     <DefaultPage title="FindPetPage">
@@ -76,6 +94,12 @@ const { category } = useParams();
         <ModalContainer />
       </Wrapper>
       <NoticeCategoriesList categoryNotice={category} items={visibleNotices} />
+
+      {isShowButtonTop && (
+        <ButtonToTop onClick={scrollTopPage}>
+          <SlArrowUp />
+        </ButtonToTop>
+      )}
     </DefaultPage>
   );
 }
--- src/pages/OurFriendsPage/OurFriendsPage.js
+++ src/pages/OurFriendsPage/OurFriendsPage.js
@@ -1,14 +1,21 @@
 import { getAllSponsors } from "../../services/apiSponsors";
 import { DefaultPage } from "../../components/DefaultPage/DefaultPage";
 import { useEffect, useState } from "react";
+import { SlArrowUp } from "react-icons/sl";
 import { SponsorsList } from "../../components/Sponsors/SponsorsList";
+import { ButtonToTop } from "../../components/ButtonToTop";
+import { scrollTopPage } from "../../helpers/scrollUp";
+
+const PAGE_SCROLL_DOWN = 600;
+
 export default function OurFriendsPage() {
   const [sponsors, setSponsors] = useState([]);
+  const [scrollTop, setScrollTop] = useState(0);
+
   useEffect(() => {
     const fechSponsors = async () => {
       try {
         const { data } = await getAllSponsors();
-        console.log(data);
         setSponsors(data);
       } catch (error) {
         console.log(error.message);
@@ -16,9 +23,30 @@ export default function OurFriendsPage() {
     };
     fechSponsors();
   }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollTop(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const isShowButtonTop = scrollTop > PAGE_SCROLL_DOWN;
+
   return (
     <DefaultPage title="Our Friends">
       <SponsorsList info={sponsors} />
+
+      {isShowButtonTop && (
+        <ButtonToTop onClick={scrollTopPage}>
+          <SlArrowUp />
+        </ButtonToTop>
+      )}
     </DefaultPage>
   );
 }
