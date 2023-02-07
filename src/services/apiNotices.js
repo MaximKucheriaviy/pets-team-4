2227dd57410fca8a1f22de280a18fdc22fc9252f
YuriiLeo <yuriileontenko@gmail.com>
2023-02-07 19:09:05 +0200

    add newAPI

--- src/services/apiNotices.js
+++ src/services/apiNotices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-// import { BACKEND_BASE_URL } from "../Constants/backendBaseUrl";
+import { BACKEND_BASE_URL } from "../Constants/backendBaseUrl";
 
-const BACKEND_BASE_URL = "http://localhost:3001";
+// const BACKEND_BASE_URL = "http://localhost:3001";
 axios.defaults.baseURL = BACKEND_BASE_URL;
 
 export const getFavorites = async (token) => {
@@ -20,5 +20,28 @@ export const getNoticesByCategory = async (category) => {
 export const getOwnerNotise = async (token) => {
   axios.defaults.headers.common["Authorization"] = token;
   const { data } = await axios.get(`/api/notices`);
+  return data;
+};
+export const addToFavorite = async (token, noticeId) => {
+  axios.defaults.headers.common["Authorization"] = token;
+  const { data } = await axios.post(
+    `api/notices/favorite/${noticeId}`
+  );
+  return data;
+};
+
+export const removeToFavorite = async (token, noticeId) => {
+  axios.defaults.headers.common["Authorization"] = token;
+  const { data } = await axios.delete(
+    `api/notices/favorite/${noticeId}`
+  );
+  return data;
+};
+
+export const removeNoticeById = async (token, noticeId) => {
+  axios.defaults.headers.common["Authorization"] = token;
+  const { data } = await axios.delete(
+    `api/notices/${noticeId}`
+  );
   return data;
 };
