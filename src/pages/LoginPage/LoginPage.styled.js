2c6e4396b12604e6d09cd8fbb2821ccb7ddf6086
VKlapan <vladimir.klapan@gmail.com>
2023-02-08 19:36:31 +0200

    fixed backgrounds

--- src/pages/LoginPage/LoginPage.styled.js
+++ src/pages/LoginPage/LoginPage.styled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 import BGdesk from "../RegisterPage/image/bg.png";
+import BGtab from "../RegisterPage/image/bg_tab.png";
+import BGmob from "../RegisterPage/image/bg_mob.png";
 
 export const Box = styled.div`
   height: calc(100vh - 74px);
@@ -8,6 +10,15 @@ export const Box = styled.div`
   @media screen and (min-width: 768px) {
     height: calc(100vh - 88px);
   }
+
+  @media screen and (min-width: 769px) and (max-width: 1280px) {
+    background-image: url(${BGtab});
+  }
+
+  @media screen and (max-width: 768px) {
+    background-image: url(${BGmob});
+  }
+
   width: 100%;
   position: absolute;
   overflow: hidden;
