a5c16519adaaa3ee99df42a4dd3fc5d70c6e6e9a
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2023-02-09 19:56:17 +0200

    cont fix2

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { Title, HeroContainer, Content, ContentImg } from "./HomePage.styled";
-import { Containers } from "./HomePage.styled";
 
 export default function HomePage() {
   return (
